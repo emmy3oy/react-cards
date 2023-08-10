4a52965120624c42718858c6a8120c9ba6c837f6
Oyase Emmanuel <114712121+emmykhalifa@users.noreply.github.com>
2023-08-10 14:48:34 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,29 +1,16 @@
 import './App.css'
-import Homepage from './pages/homepage/Homepage'
-import Signup from './pages/authentication/general-signup/Signup'
-import EmployerSignup from './pages/authentication/signup-pages/EmployerSignup'
-import JobSeekerSignup from './pages/authentication/signup-pages/JobSeekerSignup'
-import Employer from './pages/authentication/login-pages/Employer'
-import JobSeeker from './pages/authentication/login-pages/JobSeeker'
+import Homepage from './Rating/card'
+import Homepage from './Frame'
 import { Routes, Route } from 'react-router-dom'
-import JobPost from './pages/job-post/JobPost'
-import Profile from './pages/jobseeker-profile/Profile'
-import Contact from './pages/contact-us/contact'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<Homepage />} />
-        <Route exact path='/signup' element={<Signup />} />
-        <Route exact path='/signup/employer' element={<EmployerSignup />} />
-        <Route exact path='/signup/jobseeker' element={<JobSeekerSignup />} />
-        <Route exact path='/jobsboard/:jobId' element={<JobPost />} />
-        <Route exact path='/login/employer' element={<Employer />} />
-        <Route exact path='/login/jobseeker' element={<JobSeeker />} />
-        <Route exact path='/jobseeker/profile' element={<Profile />} />
-        <Route exact path='/contact-page/' element={<Contact />} />
+        <Route exact path='/' element={<Card />} />
+        <Route exact path='/frame' element={<Frame />} />
+        
       </Routes>
     </>
   );
